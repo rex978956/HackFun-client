1379991f9caeeac38053d97829c5113bf75e34da
Bandit <rex978956@gmail.com>
2020-03-15 19:26:03 +0800

    Add the course section for Home.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,8 +4,8 @@
     class="navbar"
   >
     <div class="nav-left">
-      <div class="logo">
-        Hackfun
+      <div class="logo-icon__normal">
+        <img src="../assets/images/LOGO.svg">
       </div>
     </div>
     <div class="nav-right">
@@ -38,3 +38,75 @@
     }
   };
 </script>
+
+
+<style>
+  #nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 3rem;
+    z-index: 1000;
+    background-color: #6b7bb9;
+    transition: all ease-in-out 0.3s;
+  }
+
+  #nav .nav-left {
+    height: 100%;
+    display: flex;
+    float: left;
+    align-items: center;
+    margin-left: 10%;
+  }
+
+  #nav .nav-left>.logo-icon__normal {
+    width: 100px;
+    fill: #ffffff;
+  }
+
+  #nav .nav-right {
+    height: 100%;
+    display: flex;
+    float: right;
+    align-items: center;
+    margin-right: 10%;
+  }
+
+  #nav .nav-right>.nav-menu {
+    padding-right: 100px;
+  }
+
+  #nav .nav-right>.nav-menu>.menu-item {
+    padding-left: 30px;
+    color: #ffffff;
+    text-decoration: none;
+    letter-spacing: 0;
+  }
+
+  #nav .nav-right>.nav-menu>.menu-item:hover {
+    color: #2d2e51;
+  }
+
+  #nav .nav-right>.sing {
+    padding: 6px 15px;
+    text-align: center;
+    color: #ffffff;
+    border: 1px solid #ffffff;
+    border-radius: 12px;
+    text-decoration: none;
+    letter-spacing: 0;
+  }
+
+  #nav .nav-right>.sing:hover {
+    color: #2d2e51;
+    border: 1px solid #2d2e51;
+  }
+
+  .transition-group {
+    text-align: center;
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,30 +4,111 @@
     class="main-container"
   >
     <header class="header">
-      <div class="inner-container">
-        <h1 class="title font-white">Hackfun</h1>
-        <div class="subtitle">
-          <p class="font-white">想接觸資安卻不知如何入門嗎？</p>
-          <p class="font-white">我們準備了一些教學，請找適合自己的服用 :)</p>
-        </div>
-        <h3 class="info font-white">
-          <span>* 建議熟悉基本 HTML/JS 知識，若對這塊尚不了解可參考這裡</span>
-          <a href="#">這裡</a></h3>
+      <div class="logo-title">
+        <img src="../assets/images/LOGO.svg">
+      </div>
+      <div class="subtitle">
+        <h2>想接觸資安卻不知如何入門嗎？</h2>
+        <h2>我們準備了一些教學，請找適合自己的服用 :)</h2>
+      </div>
+      <div class="info">
+        <span>* 建議熟悉基本 HTML/JS 知識，若對這塊尚不了解可參考</span>
+        <a href="#">這裡</a>
       </div>
       <div class="start">
-        <button
-          type="button"
-          class="btn_style"
-        >開始你的資安之旅！</button>
+        <a href="#course">
+          <button
+            type="button"
+            class="btn_style"
+          >開始你的資安之旅！</button></a>
       </div>
       <div class="next">
-        <img src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iaXNvLTg4NTktMSI/Pgo8IS0tIEdlbmVyYXRvcjogQWRvYmUgSWxsdXN0cmF0b3IgMTguMC4wLCBTVkcgRXhwb3J0IFBsdWctSW4gLiBTVkcgVmVyc2lvbjogNi4wMCBCdWlsZCAwKSAgLS0+CjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+CjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgdmVyc2lvbj0iMS4xIiBpZD0iQ2FwYV8xIiB4PSIwcHgiIHk9IjBweCIgdmlld0JveD0iMCAwIDU5LjQxNCA1OS40MTQiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDU5LjQxNCA1OS40MTQ7IiB4bWw6c3BhY2U9InByZXNlcnZlIiB3aWR0aD0iNTEycHgiIGhlaWdodD0iNTEycHgiPgo8cG9seWdvbiBwb2ludHM9IjU4LDE0LjE0NiAyOS43MDcsNDIuNDM5IDEuNDE0LDE0LjE0NiAwLDE1LjU2MSAyOS43MDcsNDUuMjY4IDU5LjQxNCwxNS41NjEgIiBmaWxsPSIjRkZGRkZGIi8+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+Cjwvc3ZnPgo=">
+        <img
+          src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iaXNvLTg4NTktMSI/Pgo8IS0tIEdlbmVyYXRvcjogQWRvYmUgSWxsdXN0cmF0b3IgMTguMC4wLCBTVkcgRXhwb3J0IFBsdWctSW4gLiBTVkcgVmVyc2lvbjogNi4wMCBCdWlsZCAwKSAgLS0+CjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+CjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgdmVyc2lvbj0iMS4xIiBpZD0iQ2FwYV8xIiB4PSIwcHgiIHk9IjBweCIgdmlld0JveD0iMCAwIDU5LjQxNCA1OS40MTQiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDU5LjQxNCA1OS40MTQ7IiB4bWw6c3BhY2U9InByZXNlcnZlIiB3aWR0aD0iNTEycHgiIGhlaWdodD0iNTEycHgiPgo8cG9seWdvbiBwb2ludHM9IjU4LDE0LjE0NiAyOS43MDcsNDIuNDM5IDEuNDE0LDE0LjE0NiAwLDE1LjU2MSAyOS43MDcsNDUuMjY4IDU5LjQxNCwxNS41NjEgIiBmaWxsPSIjRkZGRkZGIi8+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+CjxnPgo8L2c+Cjwvc3ZnPgo="
+        >
       </div>
     </header>
-    <div class="content-container">
-      <div class="course-area">
-        <div class="course-area-title"></div>
-
+    <div
+      id="course"
+      class="content-container"
+    >
+      <div class="course-field">
+        <h1 class="course-field-name">REVERSE</h1>
+        <p class="course-field-info">Web 領域的大致介紹，為什麼分這個領域 底下有哪些東西，常見於哪些應用</p>
+        <div class="course-item-container">
+          <div class="course-item">
+            <div class="course-name">
+              <h1>XSS</h1>
+            </div>
+            <div class="course-info-container">
+              <div class="course-info">
+                <p>跨網站腳本攻擊（Cross-site scripting，通常簡稱為 XSS 或跨站腳本）是一種網站應用程式的安全漏洞攻擊，是惡意注入攻擊的一種。</p>
+                <p>它允許惡意用戶將惡意語法注入到網頁上，使其他用戶在觀看網頁時受到影響。此網站風險是 OWAPS Top 10 常見名單之一。</p>
+              </div>
+            </div>
+          </div>
+          <div class="course-item">
+            <div class="course-name">
+              <h1>XSS</h1>
+            </div>
+            <div class="course-info-container">
+              <div class="course-info">
+                <p>跨網站腳本攻擊（Cross-site scripting，通常簡稱為 XSS 或跨站腳本）是一種網站應用程式的安全漏洞攻擊，是惡意注入攻擊的一種。</p>
+                <p>它允許惡意用戶將惡意語法注入到網頁上，使其他用戶在觀看網頁時受到影響。此網站風險是 OWAPS Top 10 常見名單之一。</p>
+              </div>
+            </div>
+          </div>
+          <div class="course-item">
+            <div class="course-name">
+              <h1>XSS</h1>
+            </div>
+            <div class="course-info-container">
+              <div class="course-info">
+                <p>跨網站腳本攻擊（Cross-site scripting，通常簡稱為 XSS 或跨站腳本）是一種網站應用程式的安全漏洞攻擊，是惡意注入攻擊的一種。</p>
+                <p>它允許惡意用戶將惡意語法注入到網頁上，使其他用戶在觀看網頁時受到影響。此網站風險是 OWAPS Top 10 常見名單之一。</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="course-field">
+        <h1 class="course-field-name">REVERSE</h1>
+        <p class="course-field-info">Web 領域的大致介紹，為什麼分這個領域 底下有哪些東西，常見於哪些應用</p>
+        <div class="course-item-container">
+          <div class="course-item">
+            <div class="course-name">
+              <h1>XSS</h1>
+            </div>
+            <div class="course-info-container">
+              <div class="course-info">
+                <p>跨網站腳本攻擊（Cross-site scripting，通常簡稱為 XSS 或跨站腳本）是一種網站應用程式的安全漏洞攻擊，是惡意注入攻擊的一種。</p>
+                <p>它允許惡意用戶將惡意語法注入到網頁上，使其他用戶在觀看網頁時受到影響。此網站風險是 OWAPS Top 10 常見名單之一。</p>
+              </div>
+            </div>
+          </div>
+          <div class="course-item">
+            <div class="course-name">
+              <h1>XSS</h1>
+            </div>
+            <div class="course-info-container">
+              <div class="course-info">
+                <p>跨網站腳本攻擊（Cross-site scripting，通常簡稱為 XSS 或跨站腳本）是一種網站應用程式的安全漏洞攻擊，是惡意注入攻擊的一種。</p>
+                <p>它允許惡意用戶將惡意語法注入到網頁上，使其他用戶在觀看網頁時受到影響。此網站風險是 OWAPS Top 10 常見名單之一。</p>
+              </div>
+            </div>
+          </div>
+          <div class="course-item">
+            <div class="course-name">
+              <h1>XSS</h1>
+            </div>
+            <div class="course-info-container">
+              <div class="course-info">
+                <p>跨網站腳本攻擊（Cross-site scripting，通常簡稱為 XSS 或跨站腳本）是一種網站應用程式的安全漏洞攻擊，是惡意注入攻擊的一種。</p>
+                <p>它允許惡意用戶將惡意語法注入到網頁上，使其他用戶在觀看網頁時受到影響。此網站風險是 OWAPS Top 10 常見名單之一。</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -36,35 +117,41 @@
 <style scope>
   .header {
     width: 100%;
+    height: 100vh;
     background-color: #141430;
-    padding: 5rem 0 8rem 0;
+    padding: 4rem 0 2rem 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 
-  .title {
-    font: Bold 50px/72px Noto Sans CJK TC;
+  .logo-title {
+    width: 350px;
   }
 
   .subtitle {
-    font: Bold 27px/49px Noto Sans CJK TC;
+    font: Bold 26px/37px Noto Sans CJK TC;
+    padding: 40px 0 40px 0;
+    line-height: 1.5;
   }
-  .subtitle > p {
-    margin: 0;
-  }
+
   .info {
-    padding-top: 20px;
-    font: Bold 20px/37px Noto Sans CJK TC;
+    padding-top: 10px;
+    font: Bold 22px/32px Noto Sans CJK TC;
   }
 
-  .info > a {
+  .info>a {
     color: #b06aec;
+    text-decoration: none;
   }
 
-  .font-white {
-    color: white;
+  .info>a:hover {
+    color: #f39251;
   }
+
   .start {
-    padding-top: 4rem;
-    padding-bottom: 1rem;
+    padding-top: 3rem;
   }
 
   .btn_style {
@@ -77,6 +164,18 @@
     text-decoration: none;
     letter-spacing: 0;
     cursor: pointer;
+  }
+
+  .next {
+    width: 100%;
+    /* position: absolute; */
+    filter: drop-shadow(0 0 8px #282828);
+  }
+
+  .next img {
+    animation: downArrow 2s infinite;
+    width: 108pt;
+    height: 60pt;
   }
 
   @keyframes downArrow {
@@ -96,14 +195,62 @@
     }
   }
 
-  .next {
-    width: 100%;
-    position: absolute;
-    filter: drop-shadow(0 0 8px #282828);
+
+  .content-container {
+    padding-top: 3rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
-  .next img {
-    animation: downArrow 2s infinite;
-    width: 108pt;
-    height: 60pt;
+
+  .course-field {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .course-field-name {
+    font: Bold 60px/87px Noto Sans CJK TC;
+    border-bottom: 2px solid #ffffff;
+    padding-bottom: 8px;
+    width: 430px;
+  }
+
+  .course-field-info {
+    padding: 2rem 0 2rem 0;
+    font: 20px/36px Noto Sans CJK TC;
+    width: 400px;
+  }
+
+  .course-item-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 0 2rem 0;
+    width: 84vw;
+  }
+
+  .course-item {
+    width: 25vw;
+  }
+
+  .course-item>.course-name>h1 {
+    font: Black 36px/52px Noto Sans CJK TC;
+    padding-bottom: 2rem;
+  }
+
+  .course-info-container {
+    height: auto;
+    background-color: #000;
+    box-shadow: 15px -15px #8384A4;
+  }
+
+  .course-info-container>.course-info>p {
+    padding: 25px 20px 25px 20px;
+    text-align: left;
+    font: 16px/36px Noto Sans CJK TC;
   }
 </style>
