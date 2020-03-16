2566b8e2667c5f74fcff92157df25fd20e611640
Bandit <rex978956@gmail.com>
2020-03-17 06:17:06 +0800

    FIx link

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,7 +5,7 @@
   >
     <div class="nav-left">
       <div class="logo-icon__normal">
-        <img src="../assets/images/logo.svg">
+        <a href="/"><img src="../assets/images/logo.svg"></a>
       </div>
     </div>
     <div class="nav-right">
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
-  publicPath: process.env.NODE_ENV === 'production' ? '/HackFun-client/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+  // publicPath: process.env.NODE_ENV === 'production' ? '/HackFun-client/' : '/'
 }
