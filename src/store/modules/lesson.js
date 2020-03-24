7125b58114b17abd3aab7535252c4d9bfcd2eb4e
Bandit <rex978956@gmail.com>
2020-03-25 00:45:39 +0800

    Fix http to https

--- src/store/modules/lesson.js
+++ src/store/modules/lesson.js
@@ -22,7 +22,7 @@ const actions = {
     if (state.lesson.name === name) {
       return state.lesson
     }
-    let res = await axios.get('http://163.13.127.158:5555/api/lesson/' + name)
+    let res = await axios.get('https://163.13.127.158:5555/api/lesson/' + name)
     commit(types.LESSON, res.data.lesson)
     return res.data.lesson
   }
