3e43977c9e997bdbe8859800e9a0cae35dc4ba41
Bandit <rex978956@gmail.com>
2020-03-26 01:32:38 +0800

    Add scrollBehavior to hash

--- src/router/index.js
+++ src/router/index.js
@@ -56,7 +56,19 @@ export const routes = [
 const router = new VueRouter({
   mode: 'history',
   // base: '/HackFun-client/',
-  routes
+  routes,
+  scrollBehavior(to) {
+    if (to.hash) {
+      return {
+        selector: to.hash
+      }
+    } else {
+      return {
+        x: 0,
+        y: 0
+      }
+    }
+  }
 })
 
 export default router
